4c865a7f63eb5e41e542779829b76ca84c465f79
jamb1975 <ingenierojaviermurcia@gmail.com>
2021-01-27 16:37:59 -0500

    prototipo collatz

--- 02-conditions/collatz.ex
+++ 02-conditions/collatz.ex
@@ -1,23 +1,18 @@
 defmodule Collatz do
   def classic(n) when is_integer(n) and n>0 do
     cond do
-        n==1 -> 1# si es uno el proceso termina en 1
-        true->fcollatz1(n) # call function privada facp1
-      end
-    end
-    def fcollatz1(n) do
-      fcollatzp1(n,1,1)
-    end
-    defp fcollatzp1(n,i,acc) when is_integer(n) do
-      cond do
-        i==n->acc  #cuando llegue a devuelve acumulado
-        rem(n,2)==0->
-          IO.puts "#{n}"
-          fcollatzp1(div(n,2),i,1)
-        true->
-          IO.puts "#{n}"
-          fcollatzp1(n*3+1,i,1)
-      end
+      n==1 -> 1# si es uno el proceso termina en 1
+      true->fcollatzp1(n) # call function privada facp1
     end
   end
-  IO.puts Collatz.classic(7)
+
+  defp fcollatzp1(n) when is_integer(n) do
+    IO.puts "#{n}"
+    cond do
+      n==1-> :ok
+      rem(n,2)==0-> fcollatzp1(div(n,2))
+      true-> fcollatzp1(n*3+1)
+    end
+  end
+end
+IO.puts Collatz.classic(7)
