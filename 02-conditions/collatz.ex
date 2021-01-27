fdaac61def4ccd1e99b824b459c3d93adbcc6033
jamb1975 <ingenierojaviermurcia@gmail.com>
2021-01-27 16:29:43 -0500

    prototipo collatz

--- 02-conditions/collatz.ex
+++ 02-conditions/collatz.ex
@@ -8,10 +8,6 @@ defmodule Collatz do
     def fcollatz1(n) do
       fcollatzp1(n,1,1)
     end
-
-    #defp facp1(0,acc) when is_integer(acc) do
-    #  acc
-    #end
     defp fcollatzp1(n,i,acc) when is_integer(n) do
       cond do
         i==n->acc  #cuando llegue a devuelve acumulado
@@ -21,22 +17,7 @@ defmodule Collatz do
         true->
           IO.puts "#{n}"
           fcollatzp1(n*3+1,i,1)
-
-      end
-    end
-    def evaluateparimpa(x)  do
-        cond do
-        rem(x,2)==0-> div(x,2)
-        true->x*3+1
       end
     end
   end
-
-
-  IO.puts Collatz.classic(6)
-  #IO.puts("fcollatz1(0) =#{Collatz.fcollatz1(0)}")
-  #IO.puts("fcollatz1(1) =#{Collatz.fcollatz1(1)}")
-  #IO.puts("fcollatz1(2) =#{Collatz.fcollatz1(2)}")
-  #IO.puts("fcollatz1(3) =#{Collatz.fcollatz1(3)}")
-  #IO.puts("fcollatz1(4) =#{Collatz.fcollatz1(4)}")
-  #IO.puts("fcollatz1(5) =#{Collatz.fcollatz1(5)}")
+  IO.puts Collatz.classic(7)
