54d19898ac4639bb42454a3c2e3b59c846b8e7da
jamb1975 <ingenierojaviermurcia@gmail.com>
2021-01-27 20:45:17 -0500

    prototipo collatz

--- 02-conditions/collatz.ex
+++ 02-conditions/collatz.ex
@@ -1,12 +1,12 @@
 defmodule Collatz do
   def classic(n) when is_integer(n) and n>0 do
-    cond do
-      n==1 -> 1# si es uno el proceso termina en 1
-      true->fcollatzp1(n) # call function privada facp1
-    end
+    #cond do
+      #n==1 -> 1# si es uno el proceso termina en 1
+    #  true->
+      fcollatzp1(n) # call function privada facp1
+   # end
   end
-
-  defp fcollatzp1(n) when is_integer(n) do
+  defp fcollatzp1(n)  do
     IO.puts "#{n}"
     cond do
       n==1-> :ok
