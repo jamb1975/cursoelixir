51ad81d3ce46f761ba1e35ae6570e1aef8d52e03
jamb1975 <ingenierojaviermurcia@gmail.com>
2021-02-04 16:57:55 -0500

    Struct protocols, observers, comprenhesio

--- 02-conditions/enumerables/printonezero_reduce.ex
+++
@@ -1,9 +0,0 @@
-defmodule PrintOneZeroReduce do
-  def printonezeroreduce(n) when is_integer(n), do: printonezeroreducep(n,0, "1", &(&1 - &2))
-  defp printonezeroreducep(0, _, s, _), do: s
-  defp printonezeroreducep(n, acc, s, action) do
-    IO.puts(s)
-    printonezeroreducep(n-1,action.(1, acc),"#{acc}" <> "#{s}", action )
-  end
-end
-PrintOneZeroReduce.printonezeroreduce(10)
