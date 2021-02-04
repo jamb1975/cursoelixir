33f37500f39588d103e86ee7fb5216e7474fa7b4
jamb1975 <jamurcia@personalsoft.com.co>
2021-02-03 19:59:34 -0500

    Cambio en sumarray defp personal soft

--- examen1/1sumaarray.ex
+++ examen1/1sumaarray.ex
@@ -1,10 +1,11 @@
 defmodule SumaArray do
   def sumaarray(list \\ []) when is_list(list), do: sumaarrayp(list, [], 0)
-  def sumaarrayp( [], resultarray, _), do: resultarray
-  def sumaarrayp([h | t] , resultarray, acc), do: sumaarrayp(t, resultarray ++ [acc + h], acc + h)
+  defp sumaarrayp( [], resultarray, _), do: resultarray
+  defp sumaarrayp([h | t] , resultarray, acc), do: sumaarrayp(t, resultarray ++ [acc + h], acc + h)
 end
 IO.inspect SumaArray.sumaarray()
 IO.inspect SumaArray.sumaarray([])
 IO.inspect SumaArray.sumaarray([1])
 IO.inspect SumaArray.sumaarray([1, -2])
 IO.inspect SumaArray.sumaarray([1, 3, 4, -1])
+IO.inspect SumaArray.sumaarray([1, 3, 4, 7])
