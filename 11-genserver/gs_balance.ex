7d76bc481a406e9fcce143404a9463521e6fdde8
jamb1975 <jamurcia@personalsoft.com.co>
2021-02-08 13:14:30 -0500

    Gen server, typespecs 10 builder

--- 11-genserver/gs_balance.ex
+++ 11-genserver/gs_balance.ex
@@ -4,17 +4,16 @@ defmodule GsBalance do
   def create(initial_state \\ 0) do
     cond do
       initial_state < 0 -> {:error, "amount can´t be negative"}
-      #true -> {:ok, spawn(__MODULE__, :listen, [initial_state])}
-      #true -> {:ok, spawn(Balance, :listen, [initial_state])}
       true -> GenServer.start_link(__MODULE__, initial_state)
 
     end
   end
 
   def init(state), do: {:ok, state}
-
+  def read(balance), do: GenServer.call(balance,{:deposit, 0})
   def deposit(balance, amount), do: GenServer.call(balance, {:deposit, amount})
   def withdraw(balance, amount), do: GenServer.call(balance, {:withdraw, amount})
+  def transfer(balance, amount), do: GenServer.cast(balance, {:transfer, amount})
 
   def handle_call({:deposit, amount}, _from_id, state) do
     state = state + amount
@@ -25,4 +24,6 @@ defmodule GsBalance do
     state = state - amount
     {:reply, state, state}
   end
+
+  def handle_cast({:transfer, amount}, state), do: {:noreply, state + amount}
 end
