043a5a72cd3138f739223c711050ecbfa4f7c77e
jamb1975 <jamurcia@personalsoft.com.co>
2021-02-10 08:25:16 -0500

    Update Ets_options

--- 06-process/ets_opts.ex
+++ 06-process/ets_opts.ex
@@ -1,4 +1,4 @@
-table = :ets.new(my_bucket,[:set, :protected]) #reference
+table = :ets.new(:my_bucket, [:set, :protected]) #reference
 :ets.insert(table, {"key", 1}) #true
 :ets.insert(table, {"key", 2}) #true
 :ets.insert(table, {"key", 3}) #true
@@ -11,7 +11,7 @@ table = :ets.new(my_bucket,[:set, :protected]) #reference
 :ets.lookup(table,"key") # [{"a", 1}, {"a", 2}, , {"a", 3}, , {"a", 4},  {"a", 5}, {"a", 7}, ]
 
 ##########################################
-table = :ets.new(my_bucket,[:bag, :protected]) #reference
+table = :ets.new(:my_bucket,[:bag, :protected]) #reference
 :ets.insert(table, {"key", 1}) #true
 :ets.insert(table, {"key", 2}) #true
 :ets.insert(table, {"key", 3}) #true
@@ -25,7 +25,7 @@ table = :ets.new(my_bucket,[:bag, :protected]) #reference
 
 ##################################################
 
-table1 = :ets.new(my_bucket,[:bag, :protected]) #reference
+table1 = :ets.new(:my_bucket,[:bag, :protected]) #reference
 :ets.insert(table, {:hello, 1}) #true
 :ets.insert(table, {:hello, 2}) #true
 :ets.insert(table, {:hello, 3}) #true
@@ -38,8 +38,13 @@ table1 = :ets.new(my_bucket,[:bag, :protected]) #reference
 :ets.lookup(table1,:hello) # [{"a", 1}, {:hello, 2}, , {:hello, 3}, {:hello, 4}, {:hello, 5}, {:hello, 7}]
 spawn_link fn -> :ets.lookup(table1, :hello)end #PID<0.115.0>
 parent = self() #PID<0.102.0>
-spawn_link fn ->send parent, :ets.lookup(table1, :hello)end #PID<0.115.0>
+spawn_link fn ->send parent, :ets.lookup(table1, :hello)end #PID<0.118.0>
 receive do
   x -> x
 end #[{"a", 1}, {:hello, 2}, , {:hello, 3}, {:hello, 4}, {:hello, 5}, {:hello, 7}]#
-spawn_link fn ->send parent, :ets.insert(table1, {:hello, 7)end #PID<0.115.0>
+spawn_link fn ->send parent, :ets.insert(table1, {:hello, 7})end #throws Argument error, cant insert  table1 is protected
+
+############################
+table1 = :ets.new(:my_bucket,[:bag, :private]) #reference
+parent = self() #PID<0.124.0>
+spawn_link fn ->send parent, :ets.lookup(table1, "key")end #throws Argument error, cant lookup table1 is private
