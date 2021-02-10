4c42107830e1a2cfca945b9259230b53628261f2
jamb1975 <jamurcia@personalsoft.com.co>
2021-02-09 20:59:43 -0500

    my_genserver gs_observer_profe

---
+++ 11-genserver/gs_observer_profe.ex
@@ -0,0 +1,57 @@
+defmodule Obs do
+  use GenServer
+  @enforce_keys :value
+  defstruct [:value, observers: []]
+  @spec create(integer()) :: pid()
+  def create(initial_value \\ 0) do
+      state = %Obs{value: initial_value}
+      GenServer.start_link(__MODULE__, state)
+  end
+
+    def init(state), do: {:ok, state}
+    defp add_observer(observers, observer_pid), do: [observer_pid | observers]
+    defp remove_observer(observers, observer_pid), do: observers -- [observer_pid]
+    defp notify(state), do: state.observers |> Enum.each(&send(&1, state.value))
+
+    def attach(subject), do: GenServer.cast(subject, {:attach, self()}) # función agregar observer
+    def detach(subject), do: GenServer.call(subject, :detach) # función remover bserver
+
+
+    def read(subject), do: GenServer.call(subject, :read)
+
+    def handle_call(:read, _reader_id, state), do: {:reply, state.value, state} # handle call remover observer
+
+    def handle_call(:detach, {observer_pid, _}, state) do
+      news_observers = state.observers |> remove_observer(observer_pid)
+      {:reply, :ok, Map.put(state, :observers, news_observers)} # handle call remover observer
+    end
+    def handle_call(:attach, {observer_pid, _}, state) do
+      news_observers = state.observers |> add_observer(observer_pid)
+      {:reply, :ok, Map.put(state, :observers, news_observers)} # handle call remover observer
+    end
+    def handle_cast(:increment, state) do
+      state = Map.put(state, :value, state.value + 1)
+      notify(state)
+      {:noreply, state}
+    end
+    def handle_cast(:decrement, state) do
+      state = Map.put(state, :value, state.value - 1)
+      notify(state)
+     {:noreply, state}
+    end
+
+    def increment(subject), do: GenServer.cast(subject, :increment)
+    def decrement(subject), do: GenServer.cast(subject, :decrement)
+
+    def await(millis \\ 1000) do
+      receive do
+        count -> count
+      after
+        millis -> :timeout
+      end
+    end
+  end
+{:ok, subject} = Obs.create(0)
+Obs.read(subject) |> IO.inspect()
+Obs.increment(subject)
+Obs.await()
