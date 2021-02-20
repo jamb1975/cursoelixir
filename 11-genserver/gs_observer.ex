ad2fe49539af0ce73b2d1f076b9c65dac2cc1974
jamb1975 <jamurcia@personalsoft.com.co>
2021-02-20 09:07:46 -0500

    Corrección Genserver observer personalizado

--- 11-genserver/gs_observer.ex
+++ 11-genserver/gs_observer.ex
@@ -9,31 +9,40 @@ defmodule GsObservable do
     end
 
     def init(state), do: {:ok, state}
-    def read(subject), do: GenServer.call(subject, :read)
+    def read(subject), do:  send(subject, {:read, subject})#GenServer.call(subject, :read)
     def increment(subject), do: GenServer.cast(subject, :increment)
     def decrement(subject), do: GenServer.cast(subject, :decrement)
-    def attach(subject), do: GenServer.cast(subject, {:attach, self()}) # función agregar observer
+    def attach(subject), do: GenServer.call(subject, {:attach, self()}) # función agregar observer
     def detach(subject), do: GenServer.call(subject, :detach) # función remover bserver
+    def notify(observers, state)  do
+     # IO.inspect observers
+      observers |> Enum.each(&send(&1, state))
+      {observers, state}
+    end  
+    
 
-
-    def handle_call(:read, _from_id, {state, observers}) do
-       {:reply, state, {state, observers}} # handle call remove observer
-
-    end
-    def handle_call(:detach, _from_id, {nil, []}), do: {:reply, {nil, []}} # handle call remover observer
+    # def handle_call(:read, from_id, {state, observers}) do
+    #   send from_id, {:state, state}
+    #   {:reply, state, {state, observers}}
+    # end
+    def handle_call(:detach, _from_id, {state, _}), do: {:reply, state, {state, []}} # handle call remover observer
 
     def handle_cast(:increment, {state, observers}) do
       state = state + 1
+      notify(observers, state)
       {:noreply, {state, observers}}
      end
 
      def handle_cast(:decrement,  {state, observers}) do
       state = state - 1
+      notify(observers, state)
       {:noreply,  {state, observers}}
-    end
+     end
 
-    def handle_cast({:attach, observer_id}, {state, observers}) do
-      {:noreply, {state, observers ++ [observer_id]}}
+    def handle_call({:attach, observer_id}, _from_id, {state, observers}) do
+   # notify(observers,state)
+   observers = observers ++ [observer_id]
+     {:reply, state, {state,observers}}
     end
 
     def await(millis \\ 1000) do
@@ -44,3 +53,16 @@ defmodule GsObservable do
       end
     end
   end
+
+{:ok, subject}  = GsObservable.create()
+subject |> IO.inspect()
+GsObservable.await |> IO.inspect()
+GsObservable.attach(subject) |> IO.inspect()
+GsObservable.detach(subject)
+GsObservable.increment(subject)
+GsObservable.await |> IO.inspect()
+GsObservable.attach(subject) |> IO.inspect()
+GsObservable.increment(subject)
+GsObservable.await |> IO.inspect()
+GsObservable.decrement(subject)
+GsObservable.await |> IO.inspect()
